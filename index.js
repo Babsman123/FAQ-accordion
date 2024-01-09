5e4731d0060f796d07a76dd5ecc8bc7842d987ed
babsman123 <babspropertyconsult@gmail.com>
2024-01-09 23:31:38 +0100

    another commit

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 // alert("welcome bro, do the job today pls");
 
-const openFaq = document.querySelector(".details-news");
-const openFisrtNews = document.querySelector(".answer-faq");
-const iconPlus = document.querySelector(".icon-plus");
-const iconMinus = document.querySelector(".icon-minus");
+const openFaq = document.querySelectorAll(".details-news");
+const openFirstNews = document.querySelectorAll(".answer-faq");
+const iconPlus = document.querySelectorAll(".icon-plus");
+const iconMinus = document.querySelectorAll(".icon-minus");
 
-openFaq.addEventListener("click", () => {
-  //   if (openFisrtNews.style.display == "none") {
-  //     openFisrtNews.style.display = "block";
-  //     console.log("hello world");
-  //   }
-  //   openFisrtNews.style.display = "block";
-  openFisrtNews.classList.toggle("answer-faq");
-  console.log(openFisrtNews.textContent);
-});
-
-// openFisrtNews.addEventListener("click", () => {
-//   openFisrtNews.style.display = "none";
-// });
+// console.log(openFirstNews[3].textContent);
+for (let i = 0; i <= openFaq.length; i++) {
+  openFaq[0].addEventListener("click", () => {
+    console.log("hello world");
+    iconPlus[0].classList.toggle("icon-plus-open");
+    iconMinus[0].classList.toggle("icon-minus-open");
+    openFirstNews[0].classList.toggle("answer-faq-open");
+  });
+  openFaq[1].addEventListener("click", () => {
+    console.log("second clcik");
+    iconPlus[1].classList.toggle("icon-plus-open");
+    iconMinus[1].classList.toggle("icon-minus-open");
+    openFirstNews[1].classList.toggle("answer-faq-open");
+  });
+  openFaq[2].addEventListener("click", () => {
+    iconPlus[2].classList.toggle("icon-plus-open");
+    iconMinus[2].classList.toggle("icon-minus-open");
+    openFirstNews[2].classList.toggle("answer-faq-open");
+  });
+  openFaq[3].addEventListener("click", () => {
+    console.log(openFirstNews[3].textContent);
+    iconPlus[3].classList.toggle("icon-plus-open");
+    iconMinus[3].classList.toggle("icon-minus-open");
+    openFirstNews[3].classList.toggle("answer-faq-open");
+  });
+}
